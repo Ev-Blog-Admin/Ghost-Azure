5f21cd40e4bc29f31fb3a0198ec5a40a1b9fda2f
Galina Borisova <borisova@mercurydevelopment.com>
2020-02-18 12:16:32 +0400

    remove error in schema context

--- core/frontend/meta/blog_logo.js
+++ core/frontend/meta/blog_logo.js
@@ -5,14 +5,7 @@ var urlUtils = require('../../server/lib/url-utils'),
 function getBlogLogo() {
     var logo = {};
 
-    if (settingsCache.get('logo')) {
-        logo.url = urlUtils.urlFor('image', {image: settingsCache.get('logo')}, true);
-    } else {
-        // CASE: no publication logo is updated. We can try to use either an uploaded publication icon
-        // or use the default one to make
-        // Google happy with it. See https://github.com/TryGhost/Ghost/issues/7558
-        logo.url = imageLib.blogIcon.getIconUrl(true);
-    }
+    logo.url = imageLib.blogIcon.getIconUrl(true);
 
     return logo;
 }
